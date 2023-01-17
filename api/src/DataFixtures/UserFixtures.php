436116f16efe1f2cae91ba3ca658eefcfafdb384
Anthony ARJONA <anthony.arjona@outlook.com>
2023-01-17 14:37:27 +0100

    add cities table

--- api/src/DataFixtures/UserFixtures.php
+++ api/src/DataFixtures/UserFixtures.php
@@ -12,14 +12,14 @@ class UserFixtures extends Fixture
     public function __construct(private UserPasswordHasherInterface $userPasswordHasher) {}
 
     public function load(ObjectManager $manager): void {
-        $users = json_decode(file_get_contents(__DIR__ . '/data/users.json'), true);
+        $datas = json_decode(file_get_contents(__DIR__ . '/data/users.json'), true);
 
-        foreach ($users as $userFixture) {
+        foreach ($datas as $data) {
 
             $user = new User();
-            $user->setEmail($userFixture['email']);
-            $user->setPassword($this->userPasswordHasher->hashPassword($user, $userFixture['password']));
-            $user->setRoles($userFixture['roles']);
+            $user->setEmail($data['email']);
+            $user->setPassword($this->userPasswordHasher->hashPassword($user, $data['password']));
+            $user->setRoles($data['roles']);
             $manager->persist($user);
         }
 
