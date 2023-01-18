1fd2028e73a214c5af1e3d5688e4ee34d4d23383
MoulinLouis <moulin.louis93@gmail.com>
2023-01-18 18:19:26 +0100

    edit User fixtures to add references

--- api/src/DataFixtures/UserFixtures.php
+++ api/src/DataFixtures/UserFixtures.php
@@ -10,29 +10,58 @@ use Faker\Factory;
 
 class UserFixtures extends Fixture
 {
-    public function __construct(private UserPasswordHasherInterface $userPasswordHasher) {}
+    public const REFERENCE = 'user-';
+    public const ENTRIES = 10;
 
-    public function load(ObjectManager $manager): void {
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
         $datas = json_decode(file_get_contents(__DIR__ . '/data/users.json'), true);
 
         $faker = Factory::create('fr_FR');
 
-        foreach ($datas as $data) {
+        foreach ($datas as $key => $data) {
 
             $user = new User();
+            $user->setFirstname($data['firstname']);
+            $user->setLastname($data['lastname']);
+            $user->setGender($data['gender']);
             $user->setEmail($data['email']);
             $user->setPassword($this->userPasswordHasher->hashPassword($user, $data['password']));
             $user->setRoles($data['roles']);
-            $user->setIsVerified($data['isVerified'] ?? false);
+            $user->setIsVerified($data['is_verified'] ?? false);
             $manager->persist($user);
+
+            $this->setReference(self::REFERENCE . $key, $user);
+        }
+
+        for ($i = count($datas); $i < self::ENTRIES + count($datas); $i++) {
+
+            $user = new User();
+            $user->setFirstname($faker->firstName);
+            $user->setLastname($faker->lastName);
+            $user->setGender($faker->boolean);
+            $user->setEmail($faker->email);
+            $user->setPassword($this->userPasswordHasher->hashPassword($user, $faker->password));
+            $user->setRoles(['ROLE_USER']);
+            $user->setIsVerified($faker->boolean);
+            $manager->persist($user);
+
+            $this->setReference(self::REFERENCE . $i, $user);
         }
 
         $manager->flush();
     }
 
-    public function getDependencies() {}
+    public function getDependencies()
+    {
+    }
 
-    public function getOrder() {
+    public function getOrder()
+    {
         return 1;
     }
 }
