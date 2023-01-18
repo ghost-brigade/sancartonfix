79d26cd4f150a296eef78db6343de54a1e0e2f3a
Anthony ARJONA <anthony.arjona@outlook.com>
2023-01-18 14:58:46 +0100

    add migrations files

--- api/migrations/Version20230118135811.php
+++ api/migrations/Version20230118135811.php
@@ -10,40 +10,45 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230118095801 extends AbstractMigration
+final class Version20230118135811 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'base entities';
+        return '';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE category_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE housing_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE "like_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE media_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE renting_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE report_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE category (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE housing (id INT NOT NULL, owner_id UUID NOT NULL, category_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, latitude DOUBLE PRECISION DEFAULT NULL, longitude DOUBLE PRECISION DEFAULT NULL, price DOUBLE PRECISION NOT NULL, active BOOLEAN NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE category (id UUID NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN category.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE housing (id UUID NOT NULL, owner_id UUID NOT NULL, category_id UUID NOT NULL, name VARCHAR(255) NOT NULL, description TEXT DEFAULT NULL, latitude NUMERIC(10, 8) DEFAULT NULL, longitude NUMERIC(11, 8) DEFAULT NULL, price NUMERIC(10, 2) NOT NULL, active BOOLEAN NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_FB8142C37E3C61F9 ON housing (owner_id)');
         $this->addSql('CREATE INDEX IDX_FB8142C312469DE2 ON housing (category_id)');
+        $this->addSql('COMMENT ON COLUMN housing.id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN housing.owner_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('CREATE TABLE "like" (id INT NOT NULL, author_id UUID NOT NULL, housing_id INT NOT NULL, liked BOOLEAN NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN housing.category_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN housing.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE "like" (id UUID NOT NULL, author_id UUID NOT NULL, housing_id UUID NOT NULL, liked BOOLEAN NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_AC6340B3F675F31B ON "like" (author_id)');
         $this->addSql('CREATE INDEX IDX_AC6340B3AD5873E3 ON "like" (housing_id)');
+        $this->addSql('COMMENT ON COLUMN "like".id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN "like".author_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('CREATE TABLE media (id INT NOT NULL, housing_id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN "like".housing_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE media (id UUID NOT NULL, housing_id UUID NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_6A2CA10CAD5873E3 ON media (housing_id)');
-        $this->addSql('CREATE TABLE renting (id INT NOT NULL, client_id UUID NOT NULL, housing_id INT NOT NULL, date_start DATE NOT NULL, date_end DATE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN media.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN media.housing_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE renting (id VARCHAR(255) NOT NULL, client_id UUID NOT NULL, housing_id UUID NOT NULL, date_start DATE NOT NULL, date_end DATE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_13533C0F19EB6921 ON renting (client_id)');
         $this->addSql('CREATE INDEX IDX_13533C0FAD5873E3 ON renting (housing_id)');
         $this->addSql('COMMENT ON COLUMN renting.client_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN renting.housing_id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN renting.date_start IS \'(DC2Type:date_immutable)\'');
         $this->addSql('COMMENT ON COLUMN renting.date_end IS \'(DC2Type:date_immutable)\'');
-        $this->addSql('CREATE TABLE report (id INT NOT NULL, renting_id INT NOT NULL, content VARCHAR(255) NOT NULL, status BOOLEAN NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE report (id INT NOT NULL, renting_id VARCHAR(255) NOT NULL, content VARCHAR(255) NOT NULL, status BOOLEAN NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_C42F7784EC8CFBAF ON report (renting_id)');
         $this->addSql('CREATE TABLE "user" (id UUID NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, is_verified BOOLEAN NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
@@ -68,10 +73,6 @@ final class Version20230118095801 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE category_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE housing_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE "like_id_seq" CASCADE');
-        $this->addSql('DROP SEQUENCE media_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE renting_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE report_id_seq CASCADE');
         $this->addSql('ALTER TABLE housing DROP CONSTRAINT FK_FB8142C37E3C61F9');
