68a907a0205c2b5e5c2f00ab7695507a440803ec
MoulinLouis <moulin.louis93@gmail.com>
2023-01-19 12:09:45 +0100

    add Like fixtures

---
+++ api/src/DataFixtures/LikeFixtures.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Like;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class LikeFixtures extends Fixture implements DependentFixtureInterface
+{
+    public const REFERENCE = 'media-';
+    public const ENTRIES = 100;
+
+    private array $users = [];
+    private array $housings = [];
+
+    private function populate(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 12; $i++) {
+            $this->users[] = $this->getReference(UserFixtures::REFERENCE . $i);
+        }
+        for ($i = 0; $i < 4; $i++) {
+            $this->housings[] = $this->getReference(HousingFixtures::REFERENCE . $i);
+        }
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $this->populate($manager);
+
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < self::ENTRIES; $i++) {
+            shuffle($this->users);
+            shuffle($this->housings);
+            $user = $this->users[0];
+            $housing = $this->housings[0];
+
+            $like = new Like();
+            $like->setLiked($faker->boolean);
+            $like->setAuthor($user);
+            $like->setHousing($housing);
+
+            $manager->persist($like);
+
+            $this->setReference(self::REFERENCE . $i, $like);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            HousingFixtures::class,
+            UserFixtures::class,
+        ];
+    }
+
+    public function getOrder(): int
+    {
+        return 60;
+    }
+}
