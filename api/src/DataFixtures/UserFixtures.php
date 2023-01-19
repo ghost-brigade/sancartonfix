4876c6d0549905a6f15b8e66c94b5738dc7c0ae9
Julien Arbellini <arbellini.j@gmail.com>
2023-01-19 19:58:35 +0100

    Result page

--- api/src/DataFixtures/UserFixtures.php
+++ api/src/DataFixtures/UserFixtures.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker\Factory;
 
 class UserFixtures extends Fixture
 {
