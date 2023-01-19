a0fac636aa056384ad2e7a497fed12883bbc42ce
Anthony ARJONA <anthony.arjona@outlook.com>
2023-01-20 00:54:36 +0100

    add like api

---
+++ front/src/api/like.js
@@ -0,0 +1,30 @@
+import { Api } from "@/api/api";
+
+class Like extends Api {
+  constructor() {
+    super();
+    this.path = "/likes";
+  }
+
+  async findAll(page = 1, itemsPerPage = 20, filters = [], orders = {}) {
+    return await this.get(this.path, page, itemsPerPage, filters, orders);
+  }
+
+  async findOne(id) {
+    return await this.get(`${this.path}/${id}`);
+  }
+
+  async create(data) {
+    return await this.post(this.path, data);
+  }
+
+  async update(id, data) {
+    return await this.put(`${this.path}/${id}`, data);
+  }
+
+  async delete(id) {
+    return await this.delete(`${this.path}/${id}`);
+  }
+}
+
+export { Like };
