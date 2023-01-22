6c99f2a53c40cc7242c9cbb90c645c94d79ed9a1
Anthony ARJONA <anthony.arjona@outlook.com>
2023-01-22 22:42:52 +0100

    add homepage link in header

--- front/src/App.vue
+++ front/src/App.vue
@@ -7,8 +7,12 @@ import SecurityProvider from "./providers/SecurityProvider.vue";
 <template>
     <SecurityProvider>
         <header class="app-header">
-            <img src="./assets/images/logo.png" alt="logo" />
-            <img src="./assets/images/logo_text.png" alt="SanCartonFix" />
+            <router-link :to="{ name: 'home' }">
+                <img src="./assets/images/logo.png" alt="logo" />
+            </router-link>
+            <router-link :to="{ name: 'home' }">
+                <img src="./assets/images/logo_text.png" alt="SanCartonFix" />
+            </router-link>
             <MenuHeader />
         </header>
 
