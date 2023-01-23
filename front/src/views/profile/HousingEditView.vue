a208d242201d991a0a5942b3a6e712756b426ef1
Anthony ARJONA <anthony.arjona@outlook.com>
2023-01-23 01:12:13 +0100

    add housing edit view

---
+++ front/src/views/profile/HousingEditView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import {useRoute} from 'vue-router';
+import HousingEditForm from "@/components/forms/HousingEditForm.vue";
+const $routes = useRoute();
+const { id } = $routes.params;
+</script>
+
+<template>
+    <HousingEditForm :housingId="id" />
+</template>
